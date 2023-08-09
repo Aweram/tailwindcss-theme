b505ba9bfa46ad8816bc51c273ed1ea07cd8370d
PortedCheese <vladimirpeshkov5@gmail.com>
2023-08-09 14:03:15 +0300

    add colors to config app

--- stubs/frontend/config/tailwind.config.js
+++ stubs/frontend/config/tailwind.config.js
@@ -1,41 +1,62 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./vendor/portedcheese/aweram-components/src/resources/**/*.blade.php",
-  ],
-  theme: {
-      container: {
-          center: true,
-          padding: {
-              DEFAULT: '.75rem',
-              sm: '3.125rem',
-              md: '2.25rem',
-              lg: '2.75rem',
-              xl: '3.25rem',
-              '2xl': '3rem'
-          }
-      },
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/css/admin.css",
 
-      screens: {
-          'xs': "480px", // => @media (min-width: 480px) {...}
+        "./vendor/portedcheese/aweram-components/src/resources/**/*.blade.php",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '.75rem',
+                sm: '3.125rem',
+                md: '2.25rem',
+                lg: '2.75rem',
+                xl: '3.25rem',
+                '2xl': '4rem'
+            }
+        },
 
-          ...defaultTheme.screens
-      },
+        screens: {
+            'xs': "480px", // => @media (min-width: 480px) {...}
 
-      flex: {
-          '0': '0 0 auto',
-      },
+            ...defaultTheme.screens
+        },
 
-      extend: {
-          fontFamily: {
-              sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-          },
-      },
-  },
-  plugins: [],
+        flex: {
+            '0': '0 0 auto',
+        },
+
+        colors: {
+            black: colors.black,
+            white: colors.white,
+            inherit: colors.inherit,
+            current: colors.current,
+            transparent: colors.transparent,
+
+            body: 'rgba(var(--color-body), <alpha-value>)',
+            // theme colors
+            light: 'rgba(var(--color-light), <alpha-value>)',
+            dark: 'rgba(var(--color-dark), <alpha-value>)',
+            success: 'rgba(var(--color-success), <alpha-value>)',
+            danger: 'rgba(var(--color-danger), <alpha-value>)',
+            warning: 'rgba(var(--color-warning), <alpha-value>)',
+            info: 'rgba(var(--color-info), <alpha-value>)',
+            primary: 'rgba(var(--color-primary), <alpha-value>)',
+            secondary: 'rgba(var(--color-secondary), <alpha-value>)',
+        },
+
+        extend: {
+            fontFamily: {
+                'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
 }
 
