de5ae526a7c37aa75b3e513e48aaa8f052d663df
PortedCheese <vladimirpeshkov5@gmail.com>
2023-08-09 15:41:23 +0300

    init btn component

---
+++ src/resources/views/components/btn.blade.php
@@ -0,0 +1,60 @@
+@props([
+    "disabledCondition" => false,
+
+    "light" => null,
+    "outlineLight" => null,
+    "dark" => null,
+    "outlineDark" => null,
+    "success" => null,
+    "outlineSuccess" => null,
+    "danger" => null,
+    "outlineDanger" => null,
+    "warning" => null,
+    "outlineWarning" => null,
+    "info" => null,
+    "outlineInfo" => null,
+    "primary" => null,
+    "outlinePrimary" => null,
+    "secondary" => null,
+    "outlineSecondary" => null,
+])
+
+@php
+    $baseClass = "inline-flex justify-center items-center flex-nowrap border-btn";
+    $sizeClass = "h-btn px-btn-x text-btn";
+    $roundedClass = "rounded-btn";
+
+    if ($light) $colorClass = "bg-light border-light text-body hover:bg-light/75 hover:border-light/75";
+    elseif ($outlineLight) $colorClass = "bg-transparent border-primary text-body hover:bg-light/10";
+
+    elseif ($dark) $colorClass = "bg-dark border-dark text-white hover:bg-dark/75 hover:border-dark/75";
+    elseif ($outlineDark) $colorClass = "bg-transparent border-dark text-dark hover:bg-dark/10";
+
+    elseif ($success) $colorClass = "bg-success border-success text-white hover:bg-success/75 hover:border-success/75";
+    elseif ($outlineSuccess) $colorClass = "bg-transparent border-success text-success hover:bg-success/10";
+
+    elseif ($danger) $colorClass = "bg-danger border-danger text-white hover:bg-danger/75 hover:border-danger/75";
+    elseif ($outlineDanger) $colorClass = "bg-transparent border-danger text-danger hover:bg-danger/10";
+
+    elseif ($warning) $colorClass = "bg-warning border-warning text-white hover:bg-warning/75 hover:border-warning/75";
+    elseif ($outlineWarning) $colorClass = "bg-transparent border-warning text-warning hover:bg-warning/10";
+
+    elseif ($info) $colorClass = "bg-info border-info text-white hover:bg-info/75 hover:border-info/75";
+    elseif ($outlineInfo) $colorClass = "bg-transparent border-info text-info hover:bg-info/10";
+
+    elseif ($primary) $colorClass = "bg-primary border-primary text-white hover:bg-primary/75 hover:border-primary/75";
+    elseif ($outlinePrimary) $colorClass = "bg-transparent border-primary text-primary hover:bg-primary/10";
+
+    elseif ($secondary) $colorClass = "bg-secondary border-secondary text-white hover:bg-secondary/75 hover:border-secondary/75";
+    elseif ($outlineSecondary) $colorClass = "bg-transparent border-secondary text-secondary hover:bg-secondary/10";
+
+    else $colorClass = "";
+@endphp
+
+<button {{ $attributes->merge([
+    "class" => implode(" ", [$baseClass, $sizeClass, $roundedClass, $colorClass]),
+    "type" => "button",
+    "disabled" => $disabledCondition
+]) }}>
+    {{ $slot }}
+</button>
--- src/resources/views/styles.blade.php
+++ src/resources/views/styles.blade.php
@@ -1,12 +1,35 @@
 <div class="container">
-    <div class="flex justify-between flex-wrap bg-white rounded p-4 text-xs font-medium text-white">
-        <x-wrmc::theme-color class="bg-light text-dark border-[#BBB8C7]">light</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-dark border-dark">dark</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-success border-success">success</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-danger border-danger">danger</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-warning border-warning text-dark">warning</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-info border-info text-dark">info</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-primary border-primary">primary</x-wrmc::theme-color>
-        <x-wrmc::theme-color class="bg-secondary border-secondary">secondary</x-wrmc::theme-color>
+    <div class="space-y-3">
+        <div class="flex justify-between flex-wrap bg-white rounded p-4 text-xs font-medium text-white">
+            <x-wrmc::theme-color class="bg-light text-dark border-[#BBB8C7]">light</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-dark border-dark">dark</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-success border-success">success</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-danger border-danger">danger</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-warning border-warning text-dark">warning</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-info border-info text-dark">info</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-primary border-primary">primary</x-wrmc::theme-color>
+            <x-wrmc::theme-color class="bg-secondary border-secondary">secondary</x-wrmc::theme-color>
+        </div>
+
+        <div class="flex justify-between flex-wrap bg-white rounded p-4">
+            <x-wrmc::btn light>Light button</x-wrmc::btn>
+            <x-wrmc::btn dark>Dark button</x-wrmc::btn>
+            <x-wrmc::btn success>Success button</x-wrmc::btn>
+            <x-wrmc::btn danger>Danger button</x-wrmc::btn>
+            <x-wrmc::btn warning>Warning button</x-wrmc::btn>
+            <x-wrmc::btn info>Info button</x-wrmc::btn>
+            <x-wrmc::btn primary>Primary button</x-wrmc::btn>
+            <x-wrmc::btn secondary>Secondary button</x-wrmc::btn>
+        </div>
+        <div class="flex justify-between flex-wrap bg-white rounded p-4">
+            <x-wrmc::btn outlineLight>Light button</x-wrmc::btn>
+            <x-wrmc::btn outlineDark>Dark button</x-wrmc::btn>
+            <x-wrmc::btn outlineSuccess>Success button</x-wrmc::btn>
+            <x-wrmc::btn outlineDanger>Danger button</x-wrmc::btn>
+            <x-wrmc::btn outlineWarning>Warning button</x-wrmc::btn>
+            <x-wrmc::btn outlineInfo>Info button</x-wrmc::btn>
+            <x-wrmc::btn outlinePrimary>Primary button</x-wrmc::btn>
+            <x-wrmc::btn outlineSecondary>Secondary button</x-wrmc::btn>
+        </div>
     </div>
 </div>
