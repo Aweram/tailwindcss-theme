97b95bd7b0aba5c88aaaffaae125569b61237bfb
PortedCheese <vladimirpeshkov5@gmail.com>
2023-08-21 00:19:45 +0300

    cover cards to card

--- src/resources/views/styles-blocks/cards.blade.php
+++ src/resources/views/styles-blocks/cards.blade.php
@@ -1,39 +1,47 @@
 <div class="row">
     <div class="col w-full mb-indent-half">
-        <h3 class="text-3xl font-bold">Cards</h3>
-    </div>
-    <div class="col w-1/3">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Card Title</h5>
-                <h6 class="card-subtitle">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-    <div class="col w-1/3">
         <div class="card">
             <div class="card-header">
-                <h5 class="card-title mb-0">Card Title in card header</h5>
+                <h3 class="text-3xl font-bold">Cards</h3>
             </div>
             <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
+                <div class="row">
+                    <div class="col w-1/3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Card Title</h5>
+                                <h6 class="card-subtitle">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col w-1/3">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title mb-0">Card Title in card header</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
 
-    <div class="col w-1/3">
-        <div class="card">
-            <div class="card-header">
-                <h5 class="card-title mb-0">Card Title in card header</h5>
-            </div>
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div class="card-footer">
-                <a href="#" class="btn btn-primary">Go somewhere from footer</a>
+                    <div class="col w-1/3">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title mb-0">Card Title in card header</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Go somewhere from footer</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
