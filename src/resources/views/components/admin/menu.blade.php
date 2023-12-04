9a7fefca65675a77303d9b59218f67a08597e813
Labrock <vladimirpeshkov5@yandex.ru>
2023-12-04 15:47:23 +0300

    logo in aside menu

--- src/resources/views/components/admin/menu.blade.php
+++ src/resources/views/components/admin/menu.blade.php
@@ -1,5 +1,12 @@
 <nav class="beautify-scrollbar fixed top-0 bottom-0 left-0 z-admin-menu h-screen w-menu overflow-x-hidden overflow-y-auto transition duration-500 transform -translate-x-menu xl:translate-x-0 bg-white"
-     x-bind:class="{ '-translate-x-menu': ! menuIsOpen }"></nav>
+     x-bind:class="{ '-translate-x-menu': ! menuIsOpen }">
+    <div class="px-indent-half">
+        <a href="{{ \Illuminate\Support\Facades\Route::has('home') ? route('home') : '/' }}"
+           class="pl-indent-half mb-indent py-indent block border-b border-bottom border-secondary/40">
+            <img src="{{ asset("imgs/aweram/logo.png") }}" alt="Aweram" class="h-auto w-32 hidden xs:inline-block">
+        </a>
+    </div>
+</nav>
 
 <section class="fixed inset-0 h-screen bg-dark opacity-50 z-admin-menu-backdrop xl:hidden"
          x-bind:class="{ 'hidden' : ! menuIsOpen }" x-on:click="menuIsOpen = false"></section>
